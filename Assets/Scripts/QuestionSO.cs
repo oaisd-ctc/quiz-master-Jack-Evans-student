1c88c7775f6292a1583f20465f7d1273da4a5531
Jack-Evans-student <112406787+Jack-Evans-student@users.noreply.github.com>
2023-03-17 09:12:35 -0400

    Part 1

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -5,7 +5,24 @@ using UnityEngine;
 public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
-    [SerializeField] string question = "Enter new question text here";
+    [SerializeField]string question = "Enter new question text here";
+    [SerializeField]string[] answers = new string[4];
+    [SerializeField]int correctAnswerIndex;
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
 
 
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
 }
+
